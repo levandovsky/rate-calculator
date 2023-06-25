89561b970872a67a7d682a57b118bec753be9e9a
Yegor Levandovskyi <yegor.levandovskyi@gmail.com>
2023-06-25 20:11:03 +0300

    Add autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import autoprefixer from "autoprefixer";
 import svgr from "./plugins/svgr";
 
 export default defineConfig({
   plugins: [react(), svgr()],
-})
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
+    },
+  },
+});
