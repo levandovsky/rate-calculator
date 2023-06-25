0bad1d7ea47fdc9499d36a106f4d1a62cec6de11
Yegor Levandovskyi <yegor.levandovskyi@gmail.com>
2023-06-25 20:11:49 +0300

    Updat declaration files

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,2 @@
-declare module "*.svg" {
-  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-  export default content;
-}
-
+/// <reference types="./index.d.ts" />
 /// <reference types="vite/client" />
